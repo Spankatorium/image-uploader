cfaa4abc3c4170c7f30fd2f43fdeb7f2ae5cac4c
HappySpanker <happyspanker@outlook.com>
2025-11-26 22:14:07 +0100

    Adding TS + node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello, world!");
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello world" });
+});
+
+app.listen(port, () => {
+  console.log("Server running on port", port);
+});
